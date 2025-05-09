e2889da5ddb3777723c0969e1d87c7623f6627ba
El Amine Mahamoud <noah.mahamoud49@gmail.com>
2025-05-09 06:52:28 +0200

    feat: implemented method creerJoueurX() pending

--- src/main/java/com/elams/demo_morpion/ParamJoueur.java
+++ src/main/java/com/elams/demo_morpion/ParamJoueur.java
@@ -1,19 +1,24 @@
 package com.elams.demo_morpion;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class ParamJoueur {
     private boolean modeDeuxJoueurs;
     @FXML
-    private VBox vbox1;
+    private ToggleGroup nbJoueurs;
     @FXML
-    private VBox vbox2;
+    private VBox vBox1;
     @FXML
-    private TextField entree1;
+    private HBox hBoxSpecial;
+    @FXML
+    private VBox vBox2;
     @FXML
     private TextField entree2;
     @FXML
@@ -25,7 +30,8 @@ public class ParamJoueur {
     private void initialize() {
         erreurMessage.setManaged(false);
 
-        if (entree2.isDisable()) {
+        // TODO gérer le cas où c'est un seul jour et donc créer MBot
+        if (modeDeuxJoueurs) {
             entree2.setText("MBot");
         }
     }
@@ -33,14 +39,26 @@ public class ParamJoueur {
     @FXML
     private void modeUnJoueur() {
         modeDeuxJoueurs = false;
-        vbox1.setDisable(false);
-        entree1.setDisable(false);
-        entree2.setDisable(true);
+        vBox1.setDisable(false);
+        hBoxSpecial.setDisable(true);
     }
 
     @FXML
     private void modeDeuxJoueurs() {
         modeDeuxJoueurs = true;
-        vbox1.setDisable(false);
+        vBox1.setDisable(false);
+        if (hBoxSpecial.isDisable()) {
+            hBoxSpecial.setDisable(false);
+        }
+    }
+
+    @FXML
+    private void creerJoueur1(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void creerJoueur2(ActionEvent event) {
+
     }
 }
