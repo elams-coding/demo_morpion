5afde15d5e9029261c003c4bea2dc5de29b0d301
El Amine Mahamoud <noah.mahamoud49@gmail.com>
2025-05-09 03:56:13 +0200

    feat: added @FXML method signatures (jouer(), regles(), credits()) - implementation pending

--- src/main/java/com/elams/demo_morpion/Accueil.java
+++ src/main/java/com/elams/demo_morpion/Accueil.java
@@ -1,4 +1,20 @@
 package com.elams.demo_morpion;
 
+import javafx.fxml.FXML;
+
 public class Accueil {
+    @FXML
+    private void jouer() {
+
+    }
+
+    @FXML
+    private void regles() {
+
+    }
+
+    @FXML
+    private void credits() {
+
+    }
 }
