587bcfa263be695ed0efb1b74d4bfd44daa932ea
El Amine Mahamoud <noah.mahamoud49@gmail.com>
2025-05-09 02:02:54 +0200

    refractor: main window of the app

---
+++ src/main/java/com/elams/demo_morpion/Morpion.java
@@ -0,0 +1,13 @@
+package com.elams.demo_morpion;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Morpion extends Application {
+    @Override
+    public void start(Stage primaryStage) {}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
