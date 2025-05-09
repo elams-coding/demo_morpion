795240be9e3b6541a57376fdec95a5f446d50e0b
El Amine Mahamoud <noah.mahamoud49@gmail.com>
2025-05-09 04:20:05 +0200

    feat: implemented `regles()` and `credits()`

--- src/main/java/com/elams/demo_morpion/Accueil.java
+++ src/main/java/com/elams/demo_morpion/Accueil.java
@@ -1,20 +1,35 @@
 package com.elams.demo_morpion;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class Accueil {
     @FXML
-    private void jouer() {
+    private ImageView image;
+    @FXML
+    private VBox reglesTxt;
+    @FXML
+    private VBox creditsTxt;
 
+    @FXML
+    private void initialize() {
+        reglesTxt.setVisible(false);
+        creditsTxt.setVisible(false);
+    }
+    
+    @FXML
+    private void jouer() {
+        System.out.println("Bientôt");
     }
 
     @FXML
     private void regles() {
-
+        reglesTxt.setVisible(!reglesTxt.isVisible());
     }
 
     @FXML
     private void credits() {
-
+        creditsTxt.setVisible(!creditsTxt.isVisible());
     }
 }
