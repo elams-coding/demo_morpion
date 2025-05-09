f0099aab9f3c2860f7fe8bbc5da95a2ace64495a
El Amine Mahamoud <noah.mahamoud49@gmail.com>
2025-05-09 06:11:23 +0200

    feat: removed unnecessary imports and managing player mode

--- src/main/java/com/elams/demo_morpion/ParamJoueur.java
+++ src/main/java/com/elams/demo_morpion/ParamJoueur.java
@@ -3,19 +3,15 @@ package com.elams.demo_morpion;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class ParamJoueur {
+    private boolean modeDeuxJoueurs;
     @FXML
     private VBox vbox1;
     @FXML
     private VBox vbox2;
-    @FXML
-    private RadioButton solo;
-    @FXML
-    private RadioButton duo;
     @FXML
     private TextField entree1;
     @FXML
@@ -28,5 +24,17 @@ public class ParamJoueur {
     @FXML
     private void initialize() {
         erreurMessage.setManaged(false);
+    }
+
+    @FXML
+    private void modeUnJoueur() {
+        modeDeuxJoueurs = false;
+        vbox1.setDisable(false);
+    }
+
+    @FXML
+    private void modeDeuxJoueurs() {
+        modeDeuxJoueurs = true;
+        vbox1.setDisable(false);
     }
 }
