97fd6a755f76c5419333dace55e4c968208f3027
El Amine Mahamoud <noah.mahamoud49@gmail.com>
2025-05-09 17:32:23 +0200

    feat: added method to display error message, method to if name is valid or not, refractor: methods creerJoueurX()

--- src/main/java/com/elams/demo_morpion/ParamJoueur.java
+++ src/main/java/com/elams/demo_morpion/ParamJoueur.java
@@ -1,12 +1,16 @@
 package com.elams.demo_morpion;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-import java.util.Objects;
+import javafx.util.Duration;
 
 public class ParamJoueur {
     private boolean modeDeuxJoueurs;
@@ -44,6 +48,7 @@ public class ParamJoueur {
         entree2.setText("MBot");
         hBoxSpecial.setDisable(true);
         // Création du joueur IA (MBot) pour le mode solo
+        // Si vous changez le nom du bot, veillez à ce que celui-ci soit valide. cf : nomValide()
         String nom = entree2.getText();
         p2 = new Player(nom, "O");
         System.out.println("Joueur 2 : " + p2);
@@ -62,14 +67,70 @@ public class ParamJoueur {
     @FXML
     private void creerJoueur1(ActionEvent event) {
         String nom = entree1.getText();
-        p1 = new Player(nom, "X");
-        System.out.println("Joueur 1 : " + p1);
+        String erreur = nomValide(nom);
+        if (erreur != null) {
+            afficherMessageErreur(erreur);
+        } else {
+            erreurMessage.setManaged(false);
+            p1 = new Player(nom, "X");
+            System.out.println("Joueur 1 : " + p1);
+        }
     }
 
     @FXML
     private void creerJoueur2(ActionEvent event) {
         String nom = entree2.getText();
-        p2 = new Player(nom, "O");
-        System.out.println("Joueur 2 : " + p2);
+        String erreur = nomValide(nom);
+        if (erreur != null) {
+            afficherMessageErreur(erreur);
+        } else {
+            erreurMessage.setManaged(false);
+            p2 = new Player(nom, "O");
+            System.out.println("Joueur 2 : " + p2);
+        }
+    }
+
+    private String nomValide(String nom) {
+        if (nom == null || nom.trim().isEmpty()) {
+            return "Le nom du joueur est obligatoire.";
+        }
+
+        String nomFormatee = nom.trim();
+
+        for (int i = 0; i < nomFormatee.length(); i++) {
+            char c = nomFormatee.charAt(i);
+            if (c == '?' || c == '!' || c == ':' || c == ',' || c == '/' || c == '\\'
+                    || c == ';' || c == '<' || c == '>' || c == '~' || c == '#'
+                    || c == '{' || c == '}' || c == '[' || c == ']' || c == '('
+                    || c == ')' || c == '|' || c == '\'' || c == '\"') {
+                return "Le nom ne doit pas contenir \"?!:,;/\\<>~#{}[]()|'\"@\".";
+            }
+        }
+
+        if (nomFormatee.length() < 3) {
+            return "Le nom doit contenir au moins trois caractères.";
+        }
+
+        if (nomFormatee.length() >= 16) {
+            return "Le nom doit contenir au plus quinze caractères.";
+        }
+
+        return null;
+    }
+
+    private void afficherMessageErreur(String erreur) {
+        erreurMessage.setText(erreur);
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, _ -> {
+                    erreurMessage.setManaged(true);
+                    erreurMessage.setVisible(true);
+                }),
+                new KeyFrame(Duration.seconds(2), _ -> {
+                    erreurMessage.setManaged(false);
+                    erreurMessage.setVisible(false);
+                })
+        );
+        timeline.setCycleCount(1);  // Une seule exécution
+        timeline.play();
     }
 }
